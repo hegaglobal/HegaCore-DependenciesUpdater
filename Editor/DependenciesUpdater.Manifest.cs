c7fb2fb7fa2a424f73b554feb0121021da570595
Laicasaane <laicasaane@outlook.com>
2020-09-06 13:20:50 +0700

    Update

---
+++ Editor/DependenciesUpdater.Manifest.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HegaCore.Editor
+{
+    using JSONNode = SimpleJSON.JSONNode;
+    using JSONObject = SimpleJSON.JSONObject;
+
+    public sealed partial class DependenciesUpdater
+    {
+        [Serializable]
+        private sealed class Manifest
+        {
+            public readonly Dictionary<string, string> dependencies = new Dictionary<string, string>();
+            public readonly List<ScopedRegistry> scopedRegistries = new List<ScopedRegistry>();
+
+            public JSONNode Serialize()
+            {
+                var json = new JSONObject();
+                var d = json[nameof(this.dependencies)].AsObject;
+
+                foreach (var v in this.dependencies)
+                {
+                    d[v.Key] = v.Value;
+                }
+
+                var s = json[nameof(this.scopedRegistries)].AsArray;
+
+                for (var i = 0; i < this.scopedRegistries.Count; i++)
+                {
+                    s[i] = this.scopedRegistries[i].Serialize();
+                }
+
+                return json;
+            }
+
+            public Manifest Deserialize(JSONNode node)
+            {
+                this.dependencies.Clear();
+
+                foreach (var v in node[nameof(this.dependencies)])
+                {
+                    this.dependencies[v.Key] = v.Value;
+                }
+
+                this.scopedRegistries.Clear();
+
+                foreach (var v in node[nameof(this.scopedRegistries)])
+                {
+                    this.scopedRegistries.Add(new ScopedRegistry().Deserialize(v.Value));
+                }
+
+                return this;
+            }
+        }
+
+        [Serializable]
+        private sealed class ScopedRegistry
+        {
+            public string name;
+            public string url;
+            public readonly List<string> scopes = new List<string>();
+
+            public JSONNode Serialize()
+            {
+                var json = new JSONObject();
+                json[nameof(this.name)] = this.name;
+                json[nameof(this.url)] = this.url;
+
+                var s = json[nameof(this.scopes)].AsArray;
+
+                for (var i = 0; i < this.scopes.Count; i++)
+                {
+                    s[i] = this.scopes[i];
+                }
+
+                return json;
+            }
+
+            public ScopedRegistry Deserialize(JSONNode node)
+            {
+                this.name = node[nameof(this.name)];
+                this.url = node[nameof(this.url)];
+
+                if (this.name == null)
+                    this.name = string.Empty;
+
+                if (this.url == null)
+                    this.url = string.Empty;
+
+                this.scopes.Clear();
+
+                foreach (var v in node[nameof(this.scopes)])
+                {
+                    this.scopes.Add(v.Value);
+                }
+
+                return this;
+            }
+        }
+    }
+}
---
+++ Editor/DependenciesUpdater.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace HegaCore.Editor
+{
+    public sealed partial class DependenciesUpdater : MonoBehaviour
+    {
+        [MenuItem("Tools/Update HegaCore Dependencies", priority = -1)]
+        public static void Update()
+        {
+            var manifestPath = Path.Combine(Application.dataPath, "..", "Packages", "manifest.json");
+            var filePath = Path.GetFullPath(manifestPath);
+            var json = File.ReadAllText(filePath);
+            var manifest = new Manifest().Deserialize(SimpleJSON.Parse(json));
+            Ensure(manifest);
+
+            File.WriteAllText($"{filePath}", manifest.Serialize().ToString(2));
+        }
+
+        private static void Ensure(Manifest manifest)
+        {
+            var d = manifest.dependencies;
+
+            d["com.unity.addressables"] = "1.15.1";
+            d["com.unity.scriptablebuildpipeline"] = "1.7.3";
+            d["jillejr.newtonsoft.json-for-unity"] = "12.0.301";
+
+            d["com.hegaglobal.visual-novel-data"] = "https://hegaglobal:HentaiG%40mes123456@github.com/hegaglobal/VisualNovelData.git#1.4.6";
+
+            d["com.cysharp.unitask"] = "2.0.31";
+            d["com.live2d.cubism-cubismloader"] = "4.0.106";
+            d["com.minhdu.uiman"] = "1.4.7";
+
+            d["com.grashaar.uiman-textmeshpro"] = "1.3.1";
+            d["com.grashaar.unity-googlespreadsheet"] = "1.0.0";
+            d["com.grashaar.unity-objectpooling"] = "1.3.2";
+
+            d["com.laicasaane.texttyper"] = "3.0.2";
+            d["com.laicasaane.tinycsvparser"] = "2.6.1";
+            d["com.laicasaane.unity-addressables-manager"] = "1.1.0";
+            d["com.laicasaane.unity-quastatemachine"] = "1.2.0";
+            d["com.laicasaane.unity-supplements"] = "2.3.4";
+
+            d["com.littlebigfun.addressable-importer"] = "0.9.2";
+            d["com.sabresaurus.playerprefseditor"] = "1.2.0";
+            d["com.merlin.easyeventeditor"] = "1.0.3";
+        }
+    }
+}
