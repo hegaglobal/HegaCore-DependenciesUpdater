1577ab59d7b44659998f66c19150c0c807b04bd6
Laicasaane <laicasaane@outlook.com>
2021-01-22 03:15:11 +0700

    Update

--- Editor/DependenciesUpdater.cs
+++ Editor/DependenciesUpdater.cs
@@ -40,7 +40,7 @@ namespace HegaCore.Editor
             d["com.laicasaane.unity-quastatemachine"]               = "1.2.0";
             d["com.laicasaane.unity-supplements"]                   = "2.5.3";
 
-            d["com.minhdu.uiman"]                                   = "1.4.29";
+            d["com.minhdu.uiman"]                                   = "1.4.30";
             d["com.live2d.cubism-cubismloader"]                     = "4.0.107";
 
             d["com.cysharp.unitask"]                                = "2.1.1";
