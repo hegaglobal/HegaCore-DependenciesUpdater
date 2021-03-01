241fc847933bcf32fefb0d699a2f4163707da420
Laicasaane <laicasaane@outlook.com>
2021-03-01 20:55:13 +0700

    Update

--- Editor/DependenciesUpdater.cs
+++ Editor/DependenciesUpdater.cs
@@ -50,6 +50,7 @@ namespace HegaCore.Editor
             d["com.coffee.uigradients"]                             = "1.0.0";
             d["jillejr.newtonsoft.json-for-unity"]                  = "12.0.302";
             d["com.oddworm.heapexplorer"]                           = "3.6.0";
+            d["com.needle.demystify"]                               = "1.0.1-pre.3";
 
             var registry = EnsureRegistry(manifest);
 
